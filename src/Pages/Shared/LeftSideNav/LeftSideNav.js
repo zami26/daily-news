bae0290f893e3212cfc2e23b248b50bc71c7dc4b
zami26 <arafatulislamzami@gmail.com>
2023-06-23 10:53:15 +0600

    hosting completed

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -6,7 +6,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/data-categories')
+        fetch('https://daily-news-server-mu.vercel.app/data-categories')
             .then(res => res.json())
             .then(data => setCategories(data))
     }, [])
